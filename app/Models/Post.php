30ca398ce1cf6e21238e80a4cd9570e4c24d8d76
Boshra Mahmoudi <boshra@Boshras-MBP-2.lan>
2026-03-18 01:11:21 +0000

    relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content'];//Add a fillable property so Laravel allows mass assignment
+    protected $fillable = ['title', 'content', 'user_id'];//Add a fillable property so Laravel allows mass assignment
     //Laravel protects models from unwanted data insertion
     //$fillable tells Laravel:
     //These fields are allowed to be inserted.
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+       // each post belongs to one user
+    }
 }
